LineageOS/android_packages_apps_Snap

Camera: Avoid 'takePicture()' in preview ready state
 Depending on timing 'takePicture()' could be
 triggered while the preview surface texture is
 still not available. The camera preview in such
 scenario can be started but will not run
 without a valid surface texture. Triggering
 snapshot in this case is not allowed and
 can lead to runtime exception.

CRs-Fixed: 769441
Change-Id: I6c4f013e9ca8b5903abbfe69394dc0871669d7c2

src/com/android/camera/PhotoModule.java
@@ -2097,8 +2097,11 @@ public class PhotoModule
                 || mUI.collapseCameraControls()
                 || (mCameraState == SWITCHING_CAMERA)
                 || (mCameraState == PREVIEW_STOPPED)
-                || (null == mFocusManager)) return;
+                || (null == mFocusManager)
+                || (null == mUI.getSurfaceTexture())) return;
+
         mShutterPressing = true;
+
         // Do not take the picture if there is not enough storage.
         if (mActivity.getStorageSpaceBytes() <= Storage.LOW_STORAGE_THRESHOLD_BYTES) {
             Log.i(TAG, "Not enough space or storage not ready. remaining="

TipsyOs/android_packages_apps_SnapdragonCamera

 triggered while the preview surface holder is
 without a valid surface holder. Triggering

src/com/android/camera/PhotoModule.java
@@ -2097,8 +2097,11 @@ public class PhotoModule
                 || mUI.collapseCameraControls()
                 || (mCameraState == SWITCHING_CAMERA)
                 || (mCameraState == PREVIEW_STOPPED)
-                || (null == mFocusManager)) return;
+                || (null == mFocusManager)
+                || (null == mUI.getSurfaceHolder())) return;
+
         mShutterPressing = true;
+
         // Do not take the picture if there is not enough storage.
         if (mActivity.getStorageSpaceBytes() <= Storage.LOW_STORAGE_THRESHOLD_BYTES) {
             Log.i(TAG, "Not enough space or storage not ready. remaining="